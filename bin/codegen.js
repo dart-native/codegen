5f7eaad0185f189be8486fcdd39da5b361dd9775
杨萧玉 <yulingtianxia@gmail.com>
2020-03-31 01:20:30 +0800

    Add cli code

--- bin/codegen.js
+++ bin/codegen.js
@@ -1,3 +1,57 @@
 #!/usr/bin/env node
 
-console.log('Hello CLI')
+const { program } = require('commander')
+var DNObjectiveConverter = require('../lib/objc/DNObjectiveConverter').DNObjectiveConverter
+var fs = require("fs")
+var path = require("path")
+
+function mkdirs(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true
+    } else {
+        if (mkdirs(path.dirname(dirname))) {
+            fs.mkdirSync(dirname)
+            return true
+        }
+    }
+}
+
+function recFindByExt(base, ext, files, result) {
+    files = files || fs.readdirSync(base)
+    result = result || []
+
+    files.forEach(
+        function (file) {
+            var newbase = path.join(base, file)
+            if (fs.statSync(newbase).isDirectory()) {
+                result = recFindByExt(newbase, ext, fs.readdirSync(newbase), result)
+            } else {
+                if (file.substr(-1 * (ext.length + 1)) == '.' + ext) {
+                    result.push(newbase)
+                }
+            }
+        }
+    )
+    return result
+}
+
+program.version('0.0.1')
+
+program
+    .arguments('<input>', 'Iutput directory')
+    .option('-l, --language <language>', 'Input Language')
+    .option('-o, --output <output>', 'Output directory')
+    .description('generate dart code ')
+    .action(function (input, options) {
+        var ext
+        if (options.language == 'objc') {
+            ext = 'h'
+        }
+        const dirs = recFindByExt(input, ext)
+        console.log(dirs)
+        if (options.output) {
+            mkdirs(options.output)
+        }
+    })
+
+program.parse(process.argv)
