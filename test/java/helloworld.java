f99716f3fb63fab3c9943a10875fe01e87230926
huizzzhou <zhouh1206@163.com>
2020-04-15 14:50:28 +0800

    generate jni signature

--- test/java/helloworld.java
+++ test/java/helloworld.java
@@ -1,6 +1,34 @@
-public class HelloWorld {   
-   public int getA(int a, boolean b, char c) {
-      return a;
-   }
-}
+public class HelloWord {
 
+  public int getInt(int i){
+    return 100;
+  }
+
+  public double getDouble(double b) {
+    return 100.23;
+  }
+
+  public byte getByte(byte b) {
+    return 1;
+  }
+
+  public float getFloat(float f) {
+    return 9.8f;
+  }
+
+  public char getChar(char c) {
+    return 'b';
+  }
+
+  public short getShort(short s) {
+    return 1;
+  }
+
+  public long getLong(long l) {
+    return 1;
+  }
+
+  public boolean getBool(boolean b) {
+    return false;
+  }
+}
