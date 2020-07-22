33a5bc0dd99273398cf0d72c847fb03ef4c5b730
杨萧玉 <yulingtianxia@gmail.com>
2020-07-22 20:28:15 +0800

    feat: support package and plugin. #36

--- bin/codegen.js
+++ bin/codegen.js
@@ -6,9 +6,10 @@ program.version('1.0.5')
 
 program
     .arguments('<input>', 'Iutput directory')
-    .option('-l, --language <language>', '[objc, java, auto(default)]')
+    .option('-l, --language <language>', '[objc, java, auto(default)]', 'auto')
     .option('-o, --output <output>', 'Output directory')
-    .option('-p, --package <package>', 'Generate a shareable Flutter project containing modular Dart code.')
+    .option('-t, --template <template>', 'Generate a shareable Flutter project containing modular Dart code.\nSpecify the type of project to create: [package, plugin]')
+    .option('--project-name <projectName>', 'The project name for this new Flutter project.\nThis must be a valid dart package name.')
     .description('Generate dart code from native API.')
     .action(main)
 
