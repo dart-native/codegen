fc3da10a704e1e5fdc035b209c359e4f3a58bbbe
杨萧玉 <yulingtianxia@gmail.com>
2020-11-30 13:33:39 +0800

    fix: handle results for undefined part file.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import { main } from './lib/main.js'
 
-let ocPath = 'test/objc/NetworkRequest.h'
-let javaPath = 'test/java/helloworld.java'
+let path = 'test/objc/NetworkRequest.h'
+// let path = 'test/java/helloworld.java'
 let option = {
     output: 'test/dart',
     // template: 'plugin',
     // projectName: 'testPlugin'
 }
 
-// run objc
-main(ocPath, option, true)
-// run java
-main(javaPath, option, false)
+// run
+main(path, option, true)
