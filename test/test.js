41725a5a158d4b946dc30e70b0d2f99044cab772
杨萧玉 <yulingtianxia@gmail.com>
2021-01-24 21:16:41 +0800

    fix: ci test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const fs = require("fs")
-const { program } = require('commander')
+import { readFileSync } from "fs"
+import { program } from 'commander'
 
 function compareDartFiles(f1, f2) {
-    var content1 = fs.readFileSync(f1, { encoding: 'utf8' })
-    var content2 = fs.readFileSync(f2, { encoding: 'utf8' })
+    var content1 = readFileSync(f1, { encoding: 'utf8' })
+    var content2 = readFileSync(f2, { encoding: 'utf8' })
     content1 = content1.replace(/[\r\n\ ]/g, '')
     content2 = content2.replace(/[\r\n\ ]/g, '')
     if (content1 != content2) {
