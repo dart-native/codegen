933b3333b1dec5ca0d0de420bdf07e92b023b028
杨萧玉 <yulingtianxia@gmail.com>
2021-11-13 17:23:28 +0800

    fix: nullsafety for method

--- bin/codegen.js
+++ bin/codegen.js
@@ -2,7 +2,7 @@
 import { program } from 'commander'
 import { main } from '../lib/main.js'
 
-program.version('2.0.1')
+program.version('2.0.2')
 
 program
     .arguments('<input>', 'Iutput directory')
